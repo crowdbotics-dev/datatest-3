0882c16a103dccfc64531fd9ea61e40e01416eb8
crowdbotics-dev <engineering@crowdbotics.com>
2023-12-07 18:08:31 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fghc
+
+admin.site.register(Fghc)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Fghc(models.Model):
+    "Generated Model"
+    fdhdj = models.BigIntegerField()
+    eeyeu = models.BigIntegerField()
